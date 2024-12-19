09d3b3444cfdfe511c3613a54e678bd1c3e516f6
Sumanth Achakala <sumanthbabu@gmail.com>
2024-12-19 06:42:12 +0530

    Add project files.

---
+++ Hospital_Management_System/CRUD.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CRUD.aspx.cs" Inherits="Hospital_Management_System.CRUD" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 177px;
+        }
+        .auto-style3 {
+            color: #0033CC;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class="auto-style3">&nbsp;&nbsp; Hospital Management System</span></h1>
+        </div>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">ID</td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Patient Name</td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Age</td>
+                <td>
+                    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Address</td>
+                <td>
+                    <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" Width="64px" />
+&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" Text="Delete" Width="64px" OnClick="Button2_Click" />
+&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Update" Width="64px" />
+&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="View" Width="64px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <p>
+            Enter Id For Update <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Hospital_Management_System/CRUD.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Web.DynamicData;
+
+namespace Hospital_Management_System
+{
+    public partial class CRUD : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=localhost; user=root; database=hospital_management_system; password=@.Sumanth5472; port=3306;";
+            using (MySqlConnection con = new MySqlConnection(ConnectionString))
+            {
+                string query = "INSERT INTO patients_table (ID, Patient_name, Age, Address) VALUES ('" + TextBox2.Text + "', '" + TextBox3.Text + "', '" + TextBox4.Text + "', '" + TextBox5.Text+ "');";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                con.Open(); // Corrected method casing
+                cmd.ExecuteNonQuery();
+                con.Close(); // Corrected method name
+            }
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=localhost; user=root; database=hospital_management_system; password=@.Sumanth5472; port=3306;";
+            using (MySqlConnection Con = new MySqlConnection(ConnectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM patients_table", Con))
+                using (MySqlDataAdapter da = new MySqlDataAdapter())
+                {
+                    cmd.Connection = Con;
+                    da.SelectCommand = cmd; // Corrected property name
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    GridView1.DataSource = dt; // Corrected GridView name
+                    GridView1.DataBind(); // Corrected GridView name
+                }
+            }
+
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=localhost; user=root; database=hospital_management_system; password=@.Sumanth5472; port=3306;";
+            using (MySqlConnection con = new MySqlConnection(ConnectionString))
+            {
+                string query = "DELETE FROM patients_table WHERE ID='" + TextBox1.Text + "';";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=localhost; user=root; database=hospital_management_system; password=@.Sumanth5472; port=3306;";
+            using (MySqlConnection con = new MySqlConnection(ConnectionString))
+            {
+                string query = "UPDATE patients_table SET ID='" + TextBox2.Text + "', Patient_Name='" + TextBox3.Text + "', Age='" + TextBox4.Text + "', Address='" + TextBox5.Text+ "' WHERE ID='" + TextBox1.Text+"';";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+        }
+    }
+}
